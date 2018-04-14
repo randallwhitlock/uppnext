0a582c5a70861902bda9c8e9408367a7b451d5cf
darkness198 <petrellvereen@gmail.com>
2018-04-14 10:11:54 -0400

    change react test

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 const App = () => {
-  return <div>Simple Sails-React stater</div>;
+  return <div>Simple Sails-React stater1</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
