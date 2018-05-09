0b083c4a84757ea76dde3412ec30f7890a173892
darkness198 <petrellvereen@gmail.com>
2018-05-09 02:19:26 -0400

    rm ora

--- frontend/build/build.js
+++ frontend/build/build.js
@@ -2,7 +2,7 @@ require('./check-versions')()
 
 process.env.NODE_ENV = 'production'
 
-var ora = require('ora')
+//var ora = require('ora')
 var rm = require('rimraf')
 var path = require('path')
 var chalk = require('chalk')
@@ -10,13 +10,13 @@ var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
 
-var spinner = ora('building for production...')
-spinner.start()
+// var spinner = ora('building for production...')
+// spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
   webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
+    //spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
       colors: true,
