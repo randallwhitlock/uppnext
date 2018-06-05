092c076635685c1348c55035e3b37eb78d8ad606
Randallwhitlock <randall.whitlock@live.com>
2018-06-04 22:33:56 -0400

    backend frontend updated in sync

--- backend/assets/sw.js
+++ backend/assets/sw.js
@@ -1,21 +1,21 @@
-importScripts("/precache-manifest.7f7ce00bc87d68dca504d4424198adb7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+importScripts("/precache-manifest.73b1f635cfe35ebf4d1f412e9e8ab8f9.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 /* eslint-disable */
-workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+// workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
 
-workbox.routing.registerRoute(
-  new RegExp('/'),
-  workbox.strategies.networkFirst()
-)
+// workbox.routing.registerRoute(
+//   new RegExp('/'),
+//   workbox.strategies.networkFirst()
+// )
 
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
-  workbox.strategies.staleWhileRevalidate()
-)
+// workbox.routing.registerRoute(
+//   new RegExp('.*\.js'),
+//   workbox.strategies.staleWhileRevalidate()
+// )
 
-workbox.routing.registerRoute(
-  new RegExp('(.*)/api/(.*)$'),
-  workbox.strategies.networkFirst()
-)
+// workbox.routing.registerRoute(
+//   new RegExp('(.*)/api/(.*)$'),
+//   workbox.strategies.networkFirst()
+// )
 
 
